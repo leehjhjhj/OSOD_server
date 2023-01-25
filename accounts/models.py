9155ffe59372a8d36ebabc13f97ce26e73a1c54e
leehjhjhj <tsukiakarii@naver.com>
2023-01-25 23:58:55 +0300

    로그인 구현(blacklist 전단계)

--- accounts/models.py
+++ accounts/models.py
@@ -4,4 +4,6 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.EmailField(max_length=50,unique=True)
     password = models.CharField(max_length=512)
-    username = models.CharField(max_length=10,unique=True, null=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    username = None
