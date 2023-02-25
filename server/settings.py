b099dfc85caefd81308b12e95c0793d960a0809a
leehjhjhj <tsukiakarii@naver.com>
2023-02-25 22:12:29 +0900

    마리아디비연결

--- server/settings.py
+++ server/settings.py
@@ -100,23 +100,23 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql', 
-#         'NAME': 'OSOD_server',
-#         'USER': 'root',
-#         'PASSWORD': '3kzv72nlecb326q',
-#         'HOST': 'svc.sel3.cloudtype.app',
-#         'PORT': '30157'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'OSOD_server',
+        'USER': 'root',
+        'PASSWORD': '3kzv72nlecb326q',
+        'HOST': 'svc.sel3.cloudtype.app',
+        'PORT': '30157'
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
