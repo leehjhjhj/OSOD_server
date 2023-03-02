f549627fcabebfe5e5943003f3df917130f21e44
leehjhjhj <tsukiakarii@naver.com>
2023-03-02 20:04:37 +0900

    cors

--- server/settings.py
+++ server/settings.py
@@ -24,7 +24,7 @@ ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://port-0-osod-108dypx2ale9l8kjq.sel3.cloudtype.app']
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-
+CORS_ALLOWED_ORIGINS = ["http://localhost:3080"]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
