facafced9a13a551461aac5df02c94ebed519d2b
leehjhjhj <tsukiakarii@naver.com>
2023-03-05 00:42:08 +0900

    구글 url 변경

--- accounts/views.py
+++ accounts/views.py
@@ -162,152 +162,26 @@ class CustomVerifyEmailView(APIView, CustomConfirmEmailView):
 ####################구글##########################
 #################################################
 BASE_URL = 'https://port-0-osod-108dypx2ale9l8kjq.sel3.cloudtype.app/'
-GOOGLE_CALLBACK_URI = BASE_URL + 'accounts/google/test/'
+GOOGLE_CALLBACK_URI = BASE_URL + 'accounts/google/login/'
 
 state = "vyv2dj"
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
-# 구글 로그인
-
-def google_login(request):
-    scope = "https://www.googleapis.com/auth/userinfo.email"
-    #client_id = os.environ.get("SOCIAL_AUTH_GOOGLE_CLIENT_ID")
-    client_id = get_secret("client_id")
-
-    return redirect(f"https://accounts.google.com/o/oauth2/v2/auth?client_id={client_id}&response_type=code&redirect_uri={GOOGLE_CALLBACK_URI}&scope={scope}")
-
-
-def google_callback(request):
-    #client_id = os.environ.get("SOCIAL_AUTH_GOOGLE_CLIENT_ID")
-    #client_secret = os.environ.get("SOCIAL_AUTH_GOOGLE_SECRET")
-    client_id = get_secret("client_id")
-    client_secret = get_secret("client_secret")
-    code = request.GET.get('code')
-    # 1. 받은 코드로 구글에 access token 요청
-    token_req = requests.post(f"https://oauth2.googleapis.com/token?client_id={client_id}&client_secret={client_secret}&code={code}&grant_type=authorization_code&redirect_uri={GOOGLE_CALLBACK_URI}&state={state}")
-    
-    ### 1-1. json으로 변환 & 에러 부분 파싱
-    token_req_json = token_req.json()
-    error = token_req_json.get("error")
-    
-
-    ### 1-2. 에러 발생 시 종료
-    if error is not None:
-        raise JSONDecodeError(error)
-    
-    ### 1-3. 성공 시 access_token 가져오기
-    access_token = token_req_json.get('access_token')
-
-    #################################################################
-
-    # 2. 가져온 access_token으로 이메일값을 구글에 요청
-    email_req = requests.get(f"https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={access_token}")
-    email_req_status = email_req.status_code
-    
-    ### 2-1. 에러 발생 시 400 에러 반환
-    if email_req_status != 200:
-        return JsonResponse({'err_msg': 'failed to get email'}, status=status.HTTP_400_BAD_REQUEST)
-    
-    ### 2-2. 성공 시 이메일 가져오기
-    email_req_json = email_req.json()
-    email = email_req_json.get('email')
-    
-    # return JsonResponse({'access': access_token, 'email':email})
-
-    #################################################################
-
-    # 3. 전달받은 이메일, access_token, code를 바탕으로 회원가입/로그인
-    try:
-        # 전달받은 이메일로 등록된 유저가 있는지 탐색
-        user = User.objects.get(email=email)
-        # FK로 연결되어 있는 socialaccount 테이블에서 해당 이메일의 유저가 있는지 확인
-        social_user = SocialAccount.objects.get(user=user)
-        
-        # 있는데 구글계정이 아니어도 에러
-        if social_user.provider != 'google':
-            return JsonResponse({'err_msg': 'no matching social type'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        if user.is_first:
-            user.is_first = False
-            user.save(update_fields=['is_first'])
-
-        # 이미 Google로 제대로 가입된 유저 => 로그인 & 해당 우저의 jwt 발급
-        data = {'access_token': access_token, 'code': code}
-        accept = requests.post(f"{BASE_URL}accounts/google/login/finish/", data=data)
-        
-        accept_status = accept.status_code
-        
-        # 뭔가 중간에 문제가 생기면 에러
-        if accept_status != 200:
-            return JsonResponse({'err_msg': 'failed to signin'}, status=accept_status)
-
-        accept_json = accept.json()
-        #accept_json.pop('user', None)
-        return JsonResponse(accept_json)
-    
-    except User.DoesNotExist:
-        # 전달받은 이메일로 기존에 가입된 유저가 아예 없으면 => 새로 회원가입 & 해당 유저의 jwt 발급
-        data = {'access_token': access_token, 'code': code}
-        
-        accept = requests.post(f"{BASE_URL}accounts/google/login/finish/", data=data)
-        
-        #return JsonResponse({'err_msg': f"{accept.reason}"})
-        accept_status = accept.status_code
-        # 뭔가 중간에 문제가 생기면 에러
-        if accept_status != 200:
-            return Response({'err_msg': 'failed to signup'}, status=accept_status)
-        accept_json = accept.json()
-        
-        #accept_json.pop('user', None)
-        return Response(accept_json)
-
-    except SocialAccount.DoesNotExist:
-        # User는 있는데 SocialAccount가 없을 때 (=일반회원으로 가입된 이메일일때)
-        return JsonResponse({'err_msg': 'email exists but not social user'}, status=status.HTTP_400_BAD_REQUEST)       
 
 class GetGoogleAccessView(APIView):
     def post(self, request, *args, **kwargs):
-        client_id = get_secret("client_id")
-        client_secret = get_secret("client_secret")
         access_token = request.data.get("access_token")
-        # # Google API에 access token을 요청하는 URL 생성
-        # url = "https://oauth2.googleapis.com/token"
-        # data = {
-        #     "code": code,
-        #     "client_id": client_id,
-        #     "client_secret": client_secret,
-        #     "redirect_uri": GOOGLE_CALLBACK_URI,
-        #     "grant_type": "authorization_code",
-        # }
-
-        # # Google API에 access token을 요청
-        # response = requests.post(url, data=data)
-
-        # # 응답에서 access token 추출
-        # response_data = response.json()
-        # return Response({"dd": f"{response_data}"})
-        
         email_req = requests.get(f"https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={access_token}")
         email_req_status = email_req.status_code
         
-        ### 2-1. 에러 발생 시 400 에러 반환
         if email_req_status != 200:
-            return JsonResponse({'err_msg': 'failed to get email'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        ### 2-2. 성공 시 이메일 가져오기
+            return Response({'err_msg': 'failed to get email'}, status=status.HTTP_400_BAD_REQUEST)
+    
         email_req_json = email_req.json()
         email = email_req_json.get('email')
-        
-        #################################################################
 
-        # 3. 전달받은 이메일, access_token, code를 바탕으로 회원가입/로그인
         try:
-            # 전달받은 이메일로 등록된 유저가 있는지 탐색
             user = User.objects.get(email=email)
-            # FK로 연결되어 있는 socialaccount 테이블에서 해당 이메일의 유저가 있는지 확인
             social_user = SocialAccount.objects.get(user=user)
             
-            # 있는데 구글계정이 아니어도 에러
             if social_user.provider != 'google':
                 return Response({'err_msg': 'no matching social type'}, status=status.HTTP_400_BAD_REQUEST)
             
@@ -315,37 +189,30 @@ class GetGoogleAccessView(APIView):
                 user.is_first = False
                 user.save(update_fields=['is_first'])
 
-            # 이미 Google로 제대로 가입된 유저 => 로그인 & 해당 우저의 jwt 발급
             data = {'access_token': access_token}
-            accept = requests.post(f"{BASE_URL}accounts/google/login-test/", data=data)
+            accept = requests.post(f"{BASE_URL}accounts/google/login/finish/", data=data)
             accept_status = accept.status_code
             
-            # 뭔가 중간에 문제가 생기면 에러
             if accept_status != 200:
                 return Response({'err_msg': 'failed to signin'}, status=accept_status)
 
             accept_json = accept.json()
-            #accept_json.pop('user', None)
             return Response(accept_json)
         
         except User.DoesNotExist:
-            # 전달받은 이메일로 기존에 가입된 유저가 아예 없으면 => 새로 회원가입 & 해당 유저의 jwt 발급
             data = {'access_token': access_token}
             
-            accept = requests.post(f"{BASE_URL}accounts/google/login-test/", data=data)
+            accept = requests.post(f"{BASE_URL}accounts/google/login/finish/", data=data)
             
-            #return JsonResponse({'err_msg': f"{accept.reason}"})
             accept_status = accept.status_code
-            # 뭔가 중간에 문제가 생기면 에러
+
             if accept_status != 200:
                 return Response({'err_msg': 'failed to signup'}, status=accept_status)
             accept_json = accept.json()
             
-            #accept_json.pop('user', None)
             return Response(accept_json)
 
         except SocialAccount.DoesNotExist:
-            # User는 있는데 SocialAccount가 없을 때 (=일반회원으로 가입된 이메일일때)
             return JsonResponse({'err_msg': 'email exists but not social user'}, status=status.HTTP_400_BAD_REQUEST)     
 
 class GoogleLogin(SocialLoginView):
