256f20d05fdda44a7aa86bd981ccd9d33f636df6
leehjhjhj <tsukiakarii@naver.com>
2023-03-08 16:05:26 +0900

    로고html화

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,14 +1,22 @@
-from django.test import TestCase
+# from django.test import TestCase
+
+# import base64
+
+# # 이미지 파일을 바이너리 모드로 읽어옵니다.
+# with open('server/static/image/logo.png', 'rb') as f:
+#     image_data = f.read()
+
+# # 이미지 데이터를 Base64로 인코딩합니다.
+# encoded_image = base64.b64encode(image_data).decode('utf-8')
+
+# # HTML 템플릿에서 사용할 수 있는 형식으로 변환합니다.
+# image_src = f"data:image/png;base64,{encoded_image}"
+# print(image_src)
+
 
 import base64
 
-# 이미지 파일을 바이너리 모드로 읽어옵니다.
-with open('server/static/image/logo.png', 'rb') as f:
-    image_data = f.read()
-
-# 이미지 데이터를 Base64로 인코딩합니다.
-encoded_image = base64.b64encode(image_data).decode('utf-8')
-
-# HTML 템플릿에서 사용할 수 있는 형식으로 변환합니다.
-image_src = f"data:image/png;base64,{encoded_image}"
-print(image_src)
+# 이미지 파일 열기
+with open('/Users/leehyunje/Postman/OSOD/server/static/image/logo.png', "rb") as image_file:
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+print(encoded_string)
