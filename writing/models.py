137c587db6d33d7291ad3096c9c7c5cfb7e8620e
leehjhjhj <tsukiakarii@naver.com>
2023-03-09 21:53:12 +0900

    불필요한 sentence model삭제

--- writing/models.py
+++ writing/models.py
@@ -5,9 +5,7 @@ class Sentence(models.Model):
     sentence = models.CharField(max_length=200)
     discription = models.CharField(max_length=200)
     created_at = models.DateTimeField(null=True)
-    is_valid = models.BooleanField(default=False, null=True)
     translate = models.CharField(max_length=200, null=True)
-    source = models.CharField(max_length=200, null=True)
 
 
 class Post(models.Model):
