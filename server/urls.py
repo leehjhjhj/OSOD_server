0bc78bf9138a34a908092f132d21a2af92a7540c
leehjhjhj <tsukiakarii@naver.com>
2023-03-14 09:06:55 +0900

    메일 수정

--- server/urls.py
+++ server/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='rest_logout'),
     path('password/change/', CustomPasswordChangeView.as_view(), name='rest_password_change'),
     path('password/reset/', CustomPasswordResetView.as_view(), name='reset'),
-    path('password/reset/uid=<str:uid64>&token=<str:token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password/reset/uid=<str:uid64>/token=<str:token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     # 회원가입
     path('registration/', RegisterView.as_view(), name='rest_register'),
     
