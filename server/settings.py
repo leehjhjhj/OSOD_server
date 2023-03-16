74831ac1d52bdfe74f2ed599d4c6b3ba3648e966
leehjhjhj <tsukiakarii@naver.com>
2023-03-16 13:30:58 +0900

    change token day

--- server/settings.py
+++ server/settings.py
@@ -180,8 +180,8 @@ REST_USE_JWT = True
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(hours=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
