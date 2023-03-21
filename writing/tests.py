9c57eb6084d929f2c381d16bfd3406757b4f5fb3
leehjhjhj <tsukiakarii@naver.com>
2023-03-21 23:33:28 +0900

    구문검사 고도화

--- writing/tests.py
+++ writing/tests.py
@@ -2,15 +2,25 @@
 import spacy
 
 def is_pattern_used(sentence, pattern):
+    sentence = sentence.replace("'ve", " have").replace("'ll", " will").replace("n't", " not").replace("re", " are")
+    pattern = pattern.replace("'ve", " have").replace("'ll", " will").replace("n't", " not").replace("re", " are")
+
     nlp = spacy.load("en_core_web_sm")
-    sentence_doc = nlp(sentence.lower())
     pattern_doc = nlp(pattern.lower())
-    new_sentence = ""
+    for doc in pattern_doc:
+        if doc.lemma_ == "will":
+            sentence = sentence.replace("'d", " would")
+        elif doc.lemma_ == "have" and doc.pos_ == "AUX":
+            sentence = sentence.replace("'d", " had").replace("'s", " has")
+
+    sentence_doc = nlp(sentence.lower())
+
     new_sentence = " ".join([token.lemma_ if token.pos_ == "AUX" or token.pos_ == "VERB" else token.text for token in sentence_doc])
     new_pattern = " ".join([token.lemma_ if token.pos_ == "AUX" or token.pos_ == "VERB" else token.text for token in pattern_doc])
+
     if new_pattern in new_sentence:
         return True
     else:
         return False
     
-print(is_pattern_used("I ate lunch yesterday.", "eat lunch"))
+print(is_pattern_used("I'd be good at him", "would be good at"))
--- writing/views.py
+++ writing/views.py
@@ -60,12 +60,22 @@ def get_today_postcnt(request):
                 }, status = status.HTTP_200_OK)
 
 def is_pattern_used(sentence, pattern):
+    sentence = sentence.replace("'ve", " have").replace("'ll", " will").replace("n't", " not").replace("re", " are")
+    pattern = pattern.replace("'ve", " have").replace("'ll", " will").replace("n't", " not").replace("re", " are")
+
     nlp = spacy.load("en_core_web_sm")
-    sentence_doc = nlp(sentence.lower())
     pattern_doc = nlp(pattern.lower())
-    new_sentence = ""
+    for doc in pattern_doc:
+        if doc.lemma_ == "will":
+            sentence = sentence.replace("'d", " would")
+        elif doc.lemma_ == "have" and doc.pos_ == "AUX":
+            sentence = sentence.replace("'d", " had").replace("'s", " has")
+
+    sentence_doc = nlp(sentence.lower())
+
     new_sentence = " ".join([token.lemma_ if token.pos_ == "AUX" or token.pos_ == "VERB" else token.text for token in sentence_doc])
     new_pattern = " ".join([token.lemma_ if token.pos_ == "AUX" or token.pos_ == "VERB" else token.text for token in pattern_doc])
+
     if new_pattern in new_sentence:
         return True
     else:
