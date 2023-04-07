1c96b0929e43f7f282a3401572001ed7219e666e
leehjhjhj <tsukiakarii@naver.com>
2023-04-07 23:40:35 +0900

    자동 메일 실험

--- accounts/scheduler.py
+++ accounts/scheduler.py
@@ -1,12 +1,38 @@
 # myapp/scheduler.py
 from apscheduler.schedulers.background import BackgroundScheduler
 from .email import SubMailView
+from django.test import RequestFactory
+# class MyScheduler:
+#     def __init__(self):
+#         self.scheduler = BackgroundScheduler()
+#         self.scheduler.add_job(self.my_job, 'cron', day_of_week='*', hour=23, minute=36, second=40)
+
+#     def my_job(self):
+#         # view = SubMailView()
+#         # view.get(request=None)
+#         request = RequestFactory().get('/')
+#         SubMailView.as_view()(request)
 class MyScheduler:
     def __init__(self):
         self.scheduler = BackgroundScheduler()
-        self.scheduler.add_job(self.my_job, 'cron', day_of_week='*', hour=23, minute=32, second=00)
+        self.scheduler.add_job(
+            self.my_job,
+            'cron',
+            day_of_week='*',
+            hour=23,
+            minute=45,
+            second=00,
+            id='my_job_id'
+        )
 
     def my_job(self):
-        view = SubMailView()
-        view.get(request=None)
-        
+        try:
+            # view = SubMailView()
+            # view.get(request=None)
+            request = RequestFactory().get('/')
+            SubMailView.as_view()(request)
+        except Exception as e:
+            # 예외 처리
+            print(e)
+        finally:
+            self.scheduler.remove_job('my_job_id')
