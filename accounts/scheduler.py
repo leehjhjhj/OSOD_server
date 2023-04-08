23a1b32357b50048614149c81716f9894e9c035b
leehjhjhj <tsukiakarii@naver.com>
2023-04-08 10:01:13 +0900

    자동 메일 실험

--- accounts/scheduler.py
+++ accounts/scheduler.py
@@ -13,10 +13,10 @@ from django.http import HttpRequest
 #         # view.get(request=None)
 #         request = RequestFactory().get('/')
 #         SubMailView.as_view()(request)
-
+cnt = 0
 class MyScheduler:
+
     def __init__(self):
-        self.cnt = 0
         self.scheduler = BackgroundScheduler()
         self.is_running = False  # 스케줄러 실행 여부
         self.job_id = 'my_job_id'
@@ -24,8 +24,8 @@ class MyScheduler:
             self.my_job,
             'cron',
             day_of_week='*',
-            hour=9,
-            minute=29,
+            hour=10,
+            minute=3,
             second=00,
             id=self.job_id
         )
@@ -33,17 +33,19 @@ class MyScheduler:
         self.sub_mail_view = SubMailView()
 
     def my_job(self):
+        global cnt
         if self.is_running:  # 이미 실행중인 경우
             return
-    
+        if cnt == 1:
+            return
         try:
             self.is_running = True  # 스케줄러 실행 중으로 변경
             # request = HttpRequest()
             # self.sub_mail_view.get(request)
             self.sub_mail_view.get(request=None)
-            self.cnt += 1
-            print(self.cnt)
-            if self.cnt == 1:
+            cnt += 1
+            print(cnt)
+            if cnt == 1:
                 return
         except Exception as e:
             print(e)
