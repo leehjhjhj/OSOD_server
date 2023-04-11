996c366682905a13dc3332cd483572707f075286
leehjhjhj <tsukiakarii@naver.com>
2023-04-12 08:09:08 +0900

    scheduler test

--- accounts/scheduler.py
+++ accounts/scheduler.py
@@ -37,26 +37,29 @@ class MyScheduler:
         self.scheduler = BackgroundScheduler(max_instances=1)
         self.job_id = 'my_job_id'
         self.sub_mail_view = SubMailView()
-        
+        self.is_running = False 
         self.scheduler.add_job(
             self.my_job,
             'cron',
             day_of_week='*',
-            hour=6,
-            minute=30,
+            hour=8,
+            minute=13,
             second=00,
             id=self.job_id
         )
 
     def my_job(self):
+        if self.is_running:  # 이미 실행중인 경우
+            return
         try:
+            self.is_running = True
             self.sub_mail_view.get(request=None)
             print('SubMailView.get() function is called.')
         except Exception as e:
             print(e)
+        finally:
+            self.is_running = False
+        
 
-    def start(self):
-        self.scheduler.start()
-        print('Scheduler started.')
 
 
